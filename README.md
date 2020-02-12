03f06240df4c7dfcc1d367739732f9d5d00ce7a7
Ron Gruesbeck <rongruesbeck@gmail.com>
2020-02-12 00:38:14 -0800

    3 hours

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 ```sh
 npm start
 ```
+
+## Code
+code is in the app.js file
+
+## Next Steps if there was more time
+- Look into seemingly duplicate events (ie. same logo with same coordinates).
+- Complete the clock by updating the time state with request animation frame and format the time.
+- Use pictures in interested list and align placement
+- Add test coverage and documentation
--- app.js
+++ app.js
@@ -1,31 +1,57 @@
-import React, { useLayoutEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import EventEmitter from '@trayio/builder-squad-event-emitter';
 
-/*
-App:
-contains our visualizer board,
-interesting connectors list,
-timer, and connector sub-components
-*/
+
+// App
 class App extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             name: 'Ron',
             time: Date.now(),
-            connectors: [
-                {
-                    coords: {
-                        x: 100,
-                        y: 100
-                    },
-                    connector: {
-                        name: "test",
-                        iconURL: "test",
-                    }
-                }
-            ]
+            connectors: [],
+            list: []
         };
+    }
+
+    // update with new connectors
+    static getDerivedStateFromProps(nextProps, prevState) {
+        // generate id
+        let id = Math.random().toString(16).slice(2);
+        let newConnector = nextProps['data-tray'];
+
+        // make sure new connector has both coords and connector properties
+        if (newConnector && newConnector.coords && newConnector.connector) {
+            return {
+                connectors: [
+                    ...prevState.connectors,
+                    {
+                        ...newConnector,
+                        id: id
+                    }
+                ]
+            }
+        } else {
+            return null;
+        }
+    }
+
+
+    // handle dropping a connector into the interest list
+    handleDrop(event) {
+        let id = event.dataTransfer.getData("text");
+        let connector = this.state.connectors.find(c => c.id === id)
+
+        this.setState({
+            list: [
+                ...this.state.list,
+                connector
+            ],
+            connectors: this.state.connectors
+                        .filter(c => c.id != id)
+        })
     }
 
     render() {
@@ -33,8 +59,13 @@ class App extends React.Component {
             <div>
                 <Title name={this.state.name} />
                 <Time time={this.state.time}/>
-                <ConnectorVisualizer connectors={this.state.connectors} />
-                <ConnectorList connectors={this.state.connectors} />
+                <ConnectorVisualizer
+                    connectors={this.state.connectors}
+                />
+                <ConnectorList
+                    connectors={this.state.list}
+                    handleDrop={(event) => { this.handleDrop(event) }}
+                />
             </div>
         );
     }
@@ -51,44 +82,72 @@ const Time = ({ time }) => {
 }
 
 // Visualizer
-const ConnectorVisualizer = () => {
+const ConnectorVisualizer = ({ connectors }) => {
     let style = {
+        position: 'relative',
         width: '1000px',
-        height: '400px',
+        height: '1000px',
         border: 'dotted thin black'
     };
 
     return (
-        <div style={style}>visualizer</div>
+        <div style={style}>
+            { connectors && connectors.map(c => {
+                return <Connector
+                key={c.id}
+                id={c.id}
+                coords={c.coords}
+                connector={c.connector}
+                />
+            })}
+        </div>
     );
 }
 
 // List
 // list of selected connectors
-const ConnectorList = (connectors) => {
+const ConnectorList = ({ connectors, handleDrop }) => {
     let style = {
         border: 'dotted thin black',
-        padding: '25px'
+        height: '100px'
     }
     return (
-        <ul style={style}>
-            <li>connectors</li>
+        <ul style={style}
+            onDrop={handleDrop}
+            onDragOver={(event) => { event.preventDefault();} }>
+            { connectors && connectors
+            .map((c, idx) => {
+                return <li key={idx}>{c.connector.name}</li>
+            })}
         </ul>
     )
 }
 
 // Connector
 // connector commponent
-const Connector = ({ coords, connector }) => {
+const Connector = ({ id, coords, connector }) => {
     let style = {
+        position: 'absolute',
+        left: `${coords.x}px`,
+        top: `${coords.y}px`,
         border: 'dotted thin black',
         padding: '25px'
     }
-    return <span style={style}>{connector.name}</span>
+    return <img
+            id={id}
+            src={connector.iconURL}
+            draggable="true"
+            onDragStart={(event) => {
+                event.dataTransfer.setData("text", event.target.id)
+            }}
+            style={style}>
+            </img>
 }
 
 // render app
 ReactDOM.render(
-    <App/>,
+    <EventEmitter>
+        <App/>
+    </EventEmitter>,
     document.getElementById('root')
 );
